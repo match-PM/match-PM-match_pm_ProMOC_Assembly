3773c5897dd392a433989bca22abe2974ff2635f
BinnemannATmatch <binnemann@match.uni-hannover.de>
2025-01-02 11:08:49 +0100

    adapt the file "toolchanger_service_node.py"

--- promoc_nodes/promoc_nodes/toolchanger/toolchanger_service_node.py
+++ promoc_nodes/promoc_nodes/toolchanger/toolchanger_service_node.py
@@ -3,25 +3,35 @@ from rclpy.node import Node
 
 
 
-class Toolchanger():
+class ToolchangerServiceNode(Node):
     def __init__():
+        super().__init__("toolchanger_node")
 
         self.toolchanger_activation_server = self.create_service(ActivateToolchanger,f"{self.get_name()}/activate_toolchanger",self.callback_activate_toolchanger)
+    
+    
+    def getStatus_toolchanger(self):
+        # get the status of the toolchanger
+        pass
+        #return statusToolchanger
+
+
+    def setStatus_toolchanger(self, statusToolchanger):
+        if statusToolchanger:
+            self.get_logger().info("Switch status of toolchanger to close/active")
+            # set value of toolchager to true
+        else:
+            self.get_logger().info("Switch status of toolchanger to open")
+            # set valve of toolchager to false
         
+        statusToolchanger = self.getStatus_toolchanger()
 
-
-
-def setStatus_toolchanger(self, statusToolchanger):
-    if statusToolchanger:
-        self.get_logger().info("Switch status of toolchanger to close/active")
-    else:
-        self.get_logger().info("Switch status of toolchanger to open")
-
-    #set valve of toolchager
+        return statusToolchanger
+    
 
 
 def main(args=None):
-      
+    rclpy.init(args=args)
     node = ToolchangerServiceNode()   
     try:
         rclpy.spin(node)
